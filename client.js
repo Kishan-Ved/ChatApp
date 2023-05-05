aad095d7f1a46ec994fd3d437a2ccf840adee578
Kishan Ved <124593234+Kishan-Ved@users.noreply.github.com>
2023-05-05 18:08:56 +0530

    Add files via upload

---
+++ client.js
@@ -0,0 +1,58 @@
+
+// const express = require('express');
+// const cors = require('cors');
+
+// const app = express();
+
+// // Enable CORS for all routes
+// app.use(cors());
+
+// // Handle your routes here
+
+// app.listen(8000, () => {
+//     console.log('Server started on port 8000');
+// });
+
+const socket = io("http://localhost:8000");
+
+const form = document.getElementById("send-container");
+const messageInput = document.getElementById("messageInp");
+const messageContainer = document.querySelector(".container");
+var audio = new Audio('ting.mp3')
+
+const append = (message,position)=>{
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageElement.classList.add('message'); // Adds a class for our message element
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+    if(position=='left') {
+        audio.play();
+    }
+}
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault(); // Prevents the reloading of the page
+    const message = messageInput.value;
+    append(`You : ${message}`,'right')
+    socket.emit('send',message)
+    messageInput.value = '';
+})
+
+socket.emit('new-user-joined', prompt("Enter your name - "));
+
+socket.on('user-joined', name=>{
+    if(name!=null){
+    append(`${name} joined the chat`,'right');
+    }
+})
+
+socket.on('recieve', data=>{
+    append(`${data.name} : ${data.message}`,'left');
+})
+
+socket.on('left', name=>{
+    if(name!=null){
+    append(`${name} left the chat`,'left')
+    }
+})
---
+++ index.js
@@ -0,0 +1,31 @@
+// Node server to handle socket io connections
+
+// npm run dev - to start nodemon (run in nodeServer)
+
+// node .\index.js - maybe needed (run in nodeServer)
+
+// Contains events that happen at server level and what to do when these events happen.
+// Usually, when such events occur, functions are called at client level.
+
+const io = require('socket.io')(8000)
+
+const users = {};
+
+io.on('connection', socket=>{
+    socket.on('new-user-joined',name=>{ // If the event new-user-joined occurs, it runs the arrow function
+        // console.log(name,'joined the chat.')
+        users[socket.id] = name; // Creates a new key, socket.id is a unique id for every socket connection
+        socket.broadcast.emit('user-joined',name) // Runs for all OTHER users
+    })
+
+    socket.on('send',message=>{
+        socket.broadcast.emit('recieve',{message: message,name: users[socket.id]}) // Remember spelling of broadcast, it may cause errors.
+    })
+
+    socket.on('disconnect',message=>{
+        socket.broadcast.emit('left',users[socket.id]);
+        delete users[socket.id];
+    })
+})
+
+
