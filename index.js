3fbd020cb59b72b5fe7a490811b7fe90c5f3cc77
Kishan Ved <124593234+Kishan-Ved@users.noreply.github.com>
2023-05-09 10:07:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,28 +4,30 @@
 
 // node .\index.js - maybe needed (run in nodeServer)
 
+// CORS Extension enabled
+
 // Contains events that happen at server level and what to do when these events happen.
 // Usually, when such events occur, functions are called at client level.
 
 const io = require('socket.io')(80)
 
-const users = {};
+var users = {};
 
 io.on('connection', socket=>{
     socket.on('new-user-joined',name=>{ // If the event new-user-joined occurs, it runs the arrow function
-        // console.log(name,'joined the chat.')
-        users[socket.id] = name; // Creates a new key, socket.id is a unique id for every socket connection
         socket.broadcast.emit('user-joined',name) // Runs for all OTHER users
+        users[socket.id] = name; // Creates a new key, socket.id is a unique id for every socket connection
+        console.log(users[socket.id],'joined the chat.')
     })
 
     socket.on('send',message=>{
+        console.log(users[socket.id]," sent a message")
         socket.broadcast.emit('recieve',{message: message,name: users[socket.id]}) // Remember spelling of broadcast, it may cause errors.
     })
 
     socket.on('disconnect',message=>{
+        console.log(users[socket.id]+' left the chat.')
         socket.broadcast.emit('left',users[socket.id]);
         delete users[socket.id];
     })
 })
-
-
